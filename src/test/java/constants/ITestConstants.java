89c88fcc014a2c5ed52754cd73d607f709576e57
tanuha <asus1986@tut.by>
2021-02-07 20:22:26 +0300

    added credentials

--- src/test/java/constants/ITestConstants.java
+++ src/test/java/constants/ITestConstants.java
@@ -1,14 +1,16 @@
 package constants;
 
+import utils.PropertyReader;
+
 public interface ITestConstants {
     //for login
-    String USERNAME = "problem_user";
-    String PASSWORD = "secret_sauce";
+    String USERNAME = System.getenv().getOrDefault("username", PropertyReader.getProperty("userneme"));
+    String PASSWORD = System.getenv().getOrDefault("password", PropertyReader.getProperty("password"));
     String ERROR_MESSAGE_EMPTY = "Epic sadface: Username is required";
     String ERROR_MESSAGE_EMPTY_PASSWORD = "Epic sadface: Password is required";
     String ERROR_MESSAGE_LOGIN_OR_PASSWORD = "Epic sadface: Username and password do not match any user in this service";
 
     //for product
-    String PRODUCT_NAME = "Sauce Labs Backpack";
+    String PRODUCT_NAME = "Sauce Labs Fleece Jacket";
     String PRODUCT_NOT_FOUND = "not found";
 }
