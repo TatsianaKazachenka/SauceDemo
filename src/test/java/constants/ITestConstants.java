28ced0c29d928a5f4c4304e4948c382ea17bcc21
tanuha <asus1986@tut.by>
2021-02-07 21:28:58 +0300

    fixed name constants

--- src/test/java/constants/ITestConstants.java
+++ src/test/java/constants/ITestConstants.java
@@ -4,7 +4,7 @@ import utils.PropertyReader;
 
 public interface ITestConstants {
     //for login
-    String USERNAME = System.getenv().getOrDefault("username", PropertyReader.getProperty("userneme"));;
+    String USERNAME = System.getenv().getOrDefault("username", PropertyReader.getProperty("username"));
     String PASSWORD = System.getenv().getOrDefault("password", PropertyReader.getProperty("password"));
     String ERROR_MESSAGE_EMPTY = "Epic sadface: Username is required";
     String ERROR_MESSAGE_EMPTY_PASSWORD = "Epic sadface: Password is required";
